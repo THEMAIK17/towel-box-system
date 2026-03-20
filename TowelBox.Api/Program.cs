540263031aa2837e474b976d7586f73ed3dde3f8
armando maikol duran salas <maikold774@gmail.com>
2026-03-20 17:56:11 -0500

    fix: configure Swashbuckle Swagger UI instead of minimal OpenAPI

--- TowelBox.Api/Program.cs
+++ TowelBox.Api/Program.cs
@@ -19,15 +19,17 @@ builder.Services.AddScoped<IBoxRepository, BoxRepository>();
 builder.Services.AddScoped<ITowelService, TowelService>();
 builder.Services.AddScoped<IBoxService, BoxService>();
 builder.Services.AddControllers();
-// Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
-builder.Services.AddOpenApi();
+// Swagger / OpenAPI
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
 
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
-    app.MapOpenApi();
+    app.UseSwagger();
+    app.UseSwaggerUI();
 }
 
 app.UseHttpsRedirection();
